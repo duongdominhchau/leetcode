e4c0e9f23a717a4d476e57fd1d40b3a44cd38fe4
Chau Duong <chau.duong@ptnglobalcorp.com>
2024-03-25 20:45:06 +0700

    Add proper solution

--- src/find_all_duplicates_in_an_array.rs
+++ src/find_all_duplicates_in_an_array.rs
@@ -1,6 +1,20 @@
 // https://leetcode.com/problems/find-all-duplicates-in-an-array/
 
-pub fn find_duplicates(mut nums: Vec<i32>) -> Vec<i32> {
+pub fn find_duplicates_sign_flip(mut nums: Vec<i32>) -> Vec<i32> {
+    // Use `abs(nums[i]) - 1` as index, on first occurence, flip the number at that index.
+    // If we see a negative number at the target index, we know nums[i] appeared twice
+    let mut result = Vec::new();
+    for i in 0..nums.len() {
+        let index = (nums[i].abs() - 1) as usize;
+        if nums[index] < 0 {
+            result.push(nums[i].abs());
+        }
+        nums[index as usize] *= -1;
+    }
+    result
+}
+
+pub fn find_duplicates_stacked_count(mut nums: Vec<i32>) -> Vec<i32> {
     let p = 1_000_000;
     for i in 0..nums.len() {
         // Use `nums[index]` to count the number of occurences of index + 1, each occurence
@@ -16,6 +30,10 @@ pub fn find_duplicates(mut nums: Vec<i32>) -> Vec<i32> {
         // Map from the range 0..n back to 1..=n
         .map(|(index, _)| index as i32 + 1)
         .collect()
+}
+
+pub fn find_duplicates(nums: Vec<i32>) -> Vec<i32> {
+    find_duplicates_sign_flip(nums)
 }
 
 #[cfg(test)]
